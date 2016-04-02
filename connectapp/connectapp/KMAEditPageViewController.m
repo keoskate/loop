4c560c61e0eb3e376b87121387647862395d0af2
Xiaohan Chen <xiaohanc@usc.edu>
2016-04-01 18:19:58 -0700

    finished edit page save button disappear and appear function and alert view for data saved

--- connectapp/connectapp/KMAEditPageViewController.m
+++ connectapp/connectapp/KMAEditPageViewController.m
@@ -9,6 +9,7 @@
 #import "KMAEditPageViewController.h"
 
 @interface KMAEditPageViewController ()
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *saveButton;
 
 @end
 
@@ -16,6 +17,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    //kay's changes
+    
+    //disable save button initially
+    [self.saveButton setEnabled:NO];
+    [self.saveButton setTintColor: [UIColor clearColor]];
+    
+    //enable save button again if any text fields not empty (textfield listener)
+    [_emailField addTarget:self
+                  action:@selector(textFieldDidChange:)
+        forControlEvents:UIControlEventEditingChanged];
+    [_phoneField addTarget:self
+                    action:@selector(textFieldDidChange:)
+          forControlEvents:UIControlEventEditingChanged];
+    [_facebookField addTarget:self
+                    action:@selector(textFieldDidChange:)
+          forControlEvents:UIControlEventEditingChanged];
+    [_linkedinField addTarget:self
+                    action:@selector(textFieldDidChange:)
+          forControlEvents:UIControlEventEditingChanged];
+    [_instagramField addTarget:self
+                    action:@selector(textFieldDidChange:)
+          forControlEvents:UIControlEventEditingChanged];
+    [_snapchatField addTarget:self
+                    action:@selector(textFieldDidChange:)
+          forControlEvents:UIControlEventEditingChanged];
+    
+    
+    // end kay's changes
+    
+    
     // Do any additional setup after loading the view.
     
     self.navigationController.navigationBarHidden = NO;
@@ -39,10 +71,29 @@
     
 }
 
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+//function added by kay
+-(void) saveSuccessAlert{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Saving Information"
+                                                    message:@"Information Saved!"
+                                                   delegate:nil
+                                          cancelButtonTitle:@"Cancel"
+                                          otherButtonTitles:@"OK", nil];
+    [alert show];
+
+}
+// enables save button again when a textfield change is detected
+-(void) textFieldDidChange:(UITextField *)textField{
+    [self.saveButton setEnabled:YES];
+    [self.saveButton setTintColor:nil];
+}
+
+//end kay
 
 - (IBAction)saveButtonPressed:(id)sender{
     
@@ -50,25 +101,31 @@
   
     if (self.emailField.text.length > 0) {
         currentUser.email = self. emailField.text;
+        [self saveSuccessAlert];
     }
     if (self.phoneField.text.length > 0) {
         currentUser[@"phoneNumber"] = self.phoneField.text;
+         [self saveSuccessAlert];
     }
     if (self.facebookField.text.length > 0) {
         currentUser[@"facebookURL"] = self.facebookField.text;
+         [self saveSuccessAlert];
     }
     if (self.linkedinField.text.length > 0) {
         currentUser[@"linkedinURL"] = self.linkedinField.text;
+         [self saveSuccessAlert];
     }
     if (self.instagramField.text.length > 0) {
         currentUser[@"instagramURL"] = self.instagramField.text;
+         [self saveSuccessAlert];
     }
     if (self.snapchatField.text.length > 0) {
         currentUser[@"snapchatURL"] = self.snapchatField.text;
+         [self saveSuccessAlert];
     }
     
     NSLog(@"Saving...");
-    [currentUser saveInBackground];
+      [currentUser saveInBackground];
 }
 
 
