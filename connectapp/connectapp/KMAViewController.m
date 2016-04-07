2323d209b845e6b406b6bb5bb2a5b199e1a3a2f2
Keion Anvaripour <Keion.Anvaripour@gmail.com>
2016-04-07 14:31:02 -0700

    merged latest changed: edit, tutorial, search

--- connectapp/connectapp/KMAViewController.m
+++ connectapp/connectapp/KMAViewController.m
@@ -7,10 +7,8 @@
 //
 
 #import "KMAViewController.h"
-#import "KMATutorialView.h"
 
-@interface KMAViewController () <KMATutorialViewDelegate>
-@property KMATutorialView *tutorialView;
+@interface KMAViewController ()
 
 @end
 
@@ -22,15 +20,6 @@
     self.navigationItem.hidesBackButton = YES;
     self.navigationController.navigationBarHidden = YES;
 	// Do any additional setup after loading the view, typically from a nib.
-    
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    //if (![defaults objectForKey:@"intro_screen_viewed"]) {
-        self.tutorialView = [[KMATutorialView alloc] initWithFrame:self.view.frame];
-        
-        self.tutorialView.delegate = self;
-        self.tutorialView.backgroundColor = [UIColor whiteColor];
-        [self.view addSubview:self.tutorialView];
-    //}
 }
 
 - (void)didReceiveMemoryWarning
@@ -39,19 +28,4 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - KMATutorialViewDelegate Methods
-
--(void)doneButtonPressed{
-    
-    //    Uncomment so that the IntroView does not show after the user clicks "DONE"
-        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-        [defaults setObject:@"YES"forKey:@"intro_screen_viewed"];
-        [defaults synchronize];
-    
-    [UIView animateWithDuration:1.0 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-        self.tutorialView.alpha = 0;
-    } completion:^(BOOL finished) {
-        [self.tutorialView removeFromSuperview];
-    }];
-}
 @end
